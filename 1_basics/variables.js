146addc96c183aaa5a830ae1e4b188806eb80e47
Amitsharmaxx <160516436+Amitsharmaxx@users.noreply.github.com>
2024-07-27 10:06:40 +0000

    variables in js

---
+++ 1_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 1234;
+let accountEmail = "amit@123";
+var accountPassword ="12345";
+accountCity = "Himachal";
+
+let accountState;
+
+// now updating values
+// accountId = 23450; :- id will not update it's constant
+accountEmail = "Sharmx@123.com";
+accountPassword = "21222324";
+accountCity = "jammu";
+
+console.table([accountId , accountEmail , accountPassword , accountState]);
