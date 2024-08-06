cde631c1ae1a40713f3ce69c5091ec6eef47bda9
Amitsharmaxx <160516436+Amitsharmaxx@users.noreply.github.com>
2024-08-06 19:01:42 +0000

    conversion of Data types

---
+++ 1_basics/conversionOperations.js
@@ -0,0 +1,24 @@
+// let score ="33abc";
+
+// console.log(typeof (score));
+// console.log(typeof score);
+
+// let value =Number(score);
+// console.log(typeof value);
+
+// console.log(value);
+
+// "33" => 33
+// "33abc" => Nan (not an number)
+// true => 1 ; 
+//false => 0;
+
+let isLoggin = 0;
+let boolean = Boolean(isLoggin);
+// console.log(boolean)
+
+
+let string = 20;
+let stringNumber = String(string);
+console.log(stringNumber);
+console.log(typeof stringNumber);
