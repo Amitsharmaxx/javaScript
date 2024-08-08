7cf62eac5068fefebdf36f7be958ee4c2e6ffde5
Amitsharmaxx <160516436+Amitsharmaxx@users.noreply.github.com>
2024-08-08 10:33:54 +0000

    modify memory

--- 1_basics/dataTypes.js
+++ 1_basics/dataTypes.js
@@ -12,9 +12,18 @@
 //-----------------------------------------//
 // Data types
 
-// 1- primivite 
+// 1- primivite  (stack memory is used )
 // 7 types : string , number , boolean , null, undefined, symbol,
 
 const id= Symbol("123")
 const anotherId = Symbol("123")
 console.log(id === anotherId);
+
+
+//-------------------------------------------------------------------------------------------------------------------------//
+// memory in js 
+
+// stack (primitive )  : when we keep value in stack we get copy
+
+// heap (non - primitive ) : when we keep any value in heap we get reference
+
