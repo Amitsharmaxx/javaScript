6f5e31fb1213b449f89480d3bd4d370ac427d1b7
Amitsharmaxx <160516436+Amitsharmaxx@users.noreply.github.com>
2024-08-08 10:22:59 +0000

    modify

--- 1_basics/dataTypes.js
+++ 1_basics/dataTypes.js
@@ -1,8 +1,20 @@
 "use strict" // treat all js code as newer version
 
-console.log(3+3);
-console.log("amit");
+// console.log(3+3);
+// console.log("amit");
 
-let name = "amosh";
-let age =18;
-let isLoggedIn =false
+// let name = "amosh";
+// let age =18;
+// let isLoggedIn =false
+
+
+
+//-----------------------------------------//
+// Data types
+
+// 1- primivite 
+// 7 types : string , number , boolean , null, undefined, symbol,
+
+const id= Symbol("123")
+const anotherId = Symbol("123")
+console.log(id === anotherId);
