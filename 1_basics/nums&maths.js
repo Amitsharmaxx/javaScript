22d2f611f2b14847f12c349e06e00c68aa5a5d08
Amitsharmaxx <160516436+Amitsharmaxx@users.noreply.github.com>
2024-08-10 10:43:51 +0000

    nums and maths

---
+++ 1_basics/nums&maths.js
@@ -0,0 +1,24 @@
+const score =400 
+console.log(score);
+
+const balance = new Number(200)
+console.log(balance)
+
+//to convert currency in indian format
+const hundreds =1000000
+console.log(hundreds.toLocaleString('en-In'))
+
+
+// console.log(Math);
+// console.log(Math.abs(-4))
+
+// console.log(Math.round(4.6))
+// console.log( "Round figure of 4.2 is :-",Math.round(4.2))
+
+//  const num = console.log(Math.min(2,4,5,2,5,2,6,3,2,2,24,5,6,77,8));
+
+ 
+
+//How to generate random number using math 
+console.log(Math.floor(Math.random()*1000000)+1);
+
