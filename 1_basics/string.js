fd0c8e3cddfce0eabad583a3c1ab10a67da778dc
Amitsharmaxx <160516436+Amitsharmaxx@users.noreply.github.com>
2024-08-10 10:44:19 +0000

    string js modify

---
+++ 1_basics/string.js
@@ -0,0 +1,16 @@
+const Names = new String ("Hello-moto")
+
+console.log(Names[1])
+console.log(Names[2])
+console.log(Names[3])
+console.log(Names[4])
+console.log(Names[5])
+console.log(Names[6])
+console.log(Names[7])
+
+console.log(Names.toUpperCase())
+
+
+const newString ="  haha  haa haa  ";
+console.log(newString.trim())
+console.log(newString)
