e1e646f03c8db833f9621e6f2298136233f18039
Amitsharmaxx <160516436+Amitsharmaxx@users.noreply.github.com>
2024-08-12 06:24:15 +0000

    date time 1

---
+++ 1_basics/dates.js
@@ -0,0 +1,9 @@
+// let currentDate = new Date();
+// console.log(currentDate.toString())
+// console.log(currentDate.toDateString())
+// console.log(currentDate.toLocaleDateString())
+// console.log(currentDate.toLocaleString())
+
+let newDate = new Date("08-12-2024");
+console.log(newDate.toLocaleString())
+
